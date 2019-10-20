0f1d598f0762058fc8761523c6482c32051b0dd6
Kai <kaipeng.wu@virginpulse.com>
2019-10-20 23:50:57 +1100

    Initial project setup

---
+++ Employee App/Employee App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Employee App
+//
+//  Created by Kaipeng Wu on 20/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
